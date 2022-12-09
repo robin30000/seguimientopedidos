153aec5d6e9be179b0df29609a902de21f447b32
ROBIN <racastro218@gmail.com>
2022-12-09 10:04:46 -0500

    correccion bug

--- api/model/modelQuejasGo.php
+++ api/model/modelQuejasGo.php
@@ -93,13 +93,13 @@ class modelQuejasGo
     public function csvQuejasGo($data)
     {
         try {
-
-            $usuarioid       = $_SESSION['login'];
-            $datos           = $data;
-            $fechaini        = $datos['fechaini'];
-            $fechafin        = $datos['fechafin'];
+            $usuarioid = $data->datoslogin;
+            $usuarioid = $usuarioid['LOGIN'];
+            $datos = $data->datos;
+            $fechaini = $datos['fechaini'];
+            $fechafin = $datos['fechafin'];
             $columnaBusqueda = $datos['columnaBusqueda'];
-            $valorBusqueda   = $datos ['valorBusqueda'];
+            $valorBusqueda = $datos ['valorBusqueda'];
 
 
             if ($fechaini == "" && $fechafin == "") {
@@ -116,60 +116,36 @@ class modelQuejasGo
 
             if ($columnaBusqueda == "" || $valorBusqueda == "") {
 
-                $stmt = $this->_DB->prepare("SELECT g.id,
-                                                       g.pedido,
-                                                       g.cliente,
-                                                       g.cedtecnico,
-                                                       g.tecnico,
-                                                       g.accion,
-                                                       g.asesor,
-                                                       g.fecha,
-                                                       g.duracion,
-                                                       g.region,
-                                                       g.idllamada,
-                                                       g.observacion
-                                                FROM quejasgo g
-                                                WHERE 1 = 1
-                                                  AND g.fecha BETWEEN (:fechaini) AND (:fechafin)");
-                $stmt->execute([':fechaini' => "$fechaini 00:00:00", ':fechafin' => "$fechafin 23:59:59"]);
+                $stmt = $this->_DB->prepare("	SELECT g.id, g.pedido, g.cliente, g.cedtecnico, g.tecnico, g.accion, g.asesor, g.fecha, g.duracion, g.region, g.idllamada, g.observacion
+								FROM quejasgo g
+									WHERE 1=1
+									AND g.fecha BETWEEN (:fechaini) AND (:fechafin)
+						");
+                $stmt->execute([
+                    ':fechaini' => "$fechaini 00:00:00",
+                    ':fechafin' => "$fechafin 23-59-59",
+                ]);
 
             } else {
 
-                $stmt = $this->_DB->query("SELECT g.id,
-                                                           g.pedido,
-                                                           g.cliente,
-                                                           g.cedtecnico,
-                                                           g.tecnico,
-                                                           g.accion,
-                                                           g.asesor,
-                                                           g.fecha,
-                                                           g.duracion,
-                                                           g.region,
-                                                           g.idllamada,
-                                                           g.observacion
-                                                    FROM quejasgo g
-                                                    WHERE 1 = 1
-                                                      AND g.fecha BETWEEN ('$fechaini 00:00:00') AND ('$fechafin 23:59:59')
-                                                      AND $columnaBusqueda = '$valorBusqueda'");
-                $stmt->execute();
+                $stmt = $this->_DB->prepare("	SELECT g.id, g.pedido, g.cliente, g.cedtecnico, g.tecnico, g.accion, g.asesor, g.fecha, g.duracion, g.region, g.idllamada, g.observacion
+								FROM quejasgo g
+									WHERE 1=1
+									AND g.fecha BETWEEN (:fechaini) AND (:fechafin) AND :columnaBusqueda = :valorBusqueda
+						");
+
+                $stmt->execute([
+                    ':fechaini' => "$fechaini 00:00:00",
+                    ':fechafin' => "$fechafin 23-59-59",
+                    ':columnaBusqueda' => $columnaBusqueda,
+                    ':valorBusqueda' => $valorBusqueda,
+                ]);
 
             }
 
-
-            $queryCount = ("SELECT COUNT(pedido) AS Cantidad FROM quejasgo g
-								WHERE g.fecha BETWEEN ('$fechaini 00:00:00') AND ('$fechafin 23:59:59')");
-
-            $rr = $this->_DB->prepare($queryCount);
-            $rr->execute([':fechaini' => "$fechaini 00:00:00", ':fechafin' => "$fechafin 23:59:59"]);
-            $counter = 0;
-            if ($rr->rowCount()) {
-                $result = [];
-                if ($row = $rr->fetchAll(PDO::FETCH_ASSOC)) {
-                    $counter = $row['Cantidad'];
-                }
-
-                $fp = fopen("../tmp/$filename", 'w');
-
+            if ($stmt->rowCount()) {
+                $counter=$stmt->rowCount();
+                $fp     = fopen("../tmp/$filename", 'w');
                 $columnas = [
                     'CONSECUTIVO',
                     'PEDIDO',
@@ -184,20 +160,146 @@ class modelQuejasGo
                     'ID_LLAMADA',
                     'OBSERVACIONES',
                 ];
-
                 fputcsv($fp, $columnas);
-                fputcsv($fp, $stmt->fetchAll(PDO::FETCH_ASSOC));
+                $resultado = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                fputcsv($fp, $resultado);
                 fclose($fp);
-
-                $response = [$filename, $counter, 201];
-            } else {
-                $response = ['No se encontraron datos', 400];
+                $response = [$filename,$counter, 201];
+            }else{
+                $response=[0,203];
             }
 
-        } catch (PDOException $e) {
+        }catch (PDOException $e){
             var_dump($e->getMessage());
         }
-        $this->_DB = null;
+        $this->_BD = null;
+        echo json_encode($response);
+    }
+
+    public function buscarTecnico($data)
+    {
+        try{
+            $stmt = $this->_DB->prepare("SELECT a.nombre, a.ciudad FROM tecnicos a WHERE 1=1 AND a.identificacion = :cedula");
+
+            $stmt->execute([
+                ':cedula' => $data,
+            ]);
+            if ($stmt->rowCount()) {
+                $resultado = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                $response = [$resultado,201];
+            } else {
+                $response=0;
+            }
+        }catch (PDOException $e){
+            var_dump($e->getMessage());
+        }
+        $this->_BD = null;
+        echo json_encode($response);
+    }
+
+    public function crearTecnicoQuejasGo($data)
+    {
+
+        try{
+            $identificacion = $data['cedtecnico'];
+            $nombre         = $data['nombretecnico'];
+            $ciudad         = $data['region'];
+            $celular        = $data['celulartecnico'];
+            $empresa        = $data['empresa'];
+            $stmt = $this->_DB->prepare (" INSERT INTO tecnicos (identificacion, nombre, ciudad, celular, empresa) values ( :identificacion, UPPER(:nombre), :ciudad,:celular, :empresa)");
+            $stmt->execute([
+                ':identificacion'=>$identificacion,
+                ':nombre'=>$nombre,
+                ':ciudad'=>$ciudad,
+                ':celular'=>$celular,
+                ':empresa'=>$empresa
+            ]);
+            $response=['Usuario creado',201];
+        }catch (PDOException $e){
+            var_dump($e->getMessage());
+        }
+        $this->_BD = null;
+        echo json_encode($response);
+        //crea tecnico, activity feed
+    }
+
+    public function ciudadesQGo()
+    {
+        try{
+            $stmt = $this->_DB->query( " 	SELECT DISTINCT ciudad 
+					FROM ciudades
+					ORDER BY ciudad ASC ");
+            if ($stmt->rowCount()) {
+                $resultado = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                $response=[$resultado,201];
+            } else {
+                $response = 0;
+            } // If no records "No Content" status
+        }catch (PDOException $e){
+            var_dump($e->getMessage());
+        }
+        $this->_BD = null;
+        echo json_encode($response);
+    }
+
+    public function guardarQuejaGo($data)
+    {
+
+        try{
+            $datos       = $data->dataquejago;
+            $duracion    = $data->duracion;
+            $login       = $data->login;
+            $asesor      = $login['LOGIN'];
+            $pedido      = $datos['pedido'];
+            $cliente     = $datos['cliente'];
+            $cedtecnico  = $datos['cedtecnico'];
+            $tecnico     = $datos['tecnico'];
+            $accion      = $datos['accion'];
+            $region      = $datos['region'];
+            $idllamada   = $datos['idllamada'];
+            $observacion = utf8_decode($datos['observacion']);
+
+
+            $stmt = $this->_DB->prepare("
+					INSERT INTO quejasgo
+						(pedido, cliente, cedtecnico, tecnico, accion, asesor, fecha, duracion, region, idllamada, observacion)
+					VALUES
+						(:pedido, UPPER(TRIM(:cliente)), :cedtecnico, :tecnico, :accion, :asesor, NOW(), :duracion, :region, :idllamada, :observacion)
+				");
+            $stmt->execute([':pedido'=>$pedido,':cliente'=>$cliente,':cedtecnico'=>$cedtecnico,':tecnico'=>$tecnico,':accion'=>$accion,':asesor'=>$asesor,':duracion'=>$duracion,':region'=>$region,':idllamada'=>$idllamada,':observacion'=>$observacion]);
+
+
+            if ($stmt->rowCount()) {
+                $response=['Queja guardada',201];
+            } else {
+                $response =0;
+            }
+        }catch (PDOException $e){
+            var_dump($e->getMessage());
+        }
+
+        echo json_encode($response);
+    }
+
+    public function ActualizarObserQuejasGo($data)
+    {
+        try{
+            $observaciones = $data->observacion;
+            $idqueja       = $data->idqueja;
+
+            $stmt = $this->_DB->prepare("UPDATE quejasgo SET observacion = :observaciones where id = :idqueja");
+
+            $stmt->execute([':observaciones'=>$observaciones,':idqueja'=>$idqueja]);
+
+            if ($stmt->rowCount()) {
+                $response = ['Observacion actualizada',201];
+
+            } else {
+                $response=0;
+            }
+        }catch (PDOException $e){
+            var_dump($e->getMessage());
+        }
         echo json_encode($response);
     }
 }
