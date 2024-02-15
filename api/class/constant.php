dd1f81d0a5ee7b6fbe076106af830e4e43e150e1
Robin <racastro218@gmail.com>
2024-02-14 19:18:01 -0500

    resgistros contingencias correcciones refresh conteo se agrega datos técnicos

---
+++ api/class/constant.php
@@ -0,0 +1,2 @@
+<?php
+define('CLAVE_ENCRYPT', 'b#4rBZ4n2024');
--- app/conteoConsulta.js
+++ app/conteoConsulta.js
@@ -6,6 +6,10 @@
     function ConteoCtrl($scope, $http, $rootScope, services) {
         datos();
 
+        $scope.recargaPage = () => {
+            datos();
+        }
+
         function datos(d) {
             let data = {}
             if (d) {
@@ -41,7 +45,8 @@
                                     'rgba(255, 206, 86, 0.2)',
                                     'rgba(75, 192, 192, 0.2)',
                                     'rgba(153, 102, 255, 0.2)',
-                                    'rgba(255, 159, 64, 0.2)'
+                                    'rgba(255, 159, 64, 0.2)',
+                                    'rgba(105, 159, 64, 0.2)'
                                 ],
                                 borderColor: [
                                     'rgba(255, 99, 132, 1)',
@@ -49,7 +54,8 @@
                                     'rgba(255, 206, 86, 1)',
                                     'rgba(75, 192, 192, 1)',
                                     'rgba(153, 102, 255, 1)',
-                                    'rgba(255, 159, 64, 1)'
+                                    'rgba(255, 159, 64, 1)',
+                                    'rgba(105, 159, 64, 1)'
                                 ],
                                 borderWidth: 1
                             }]
